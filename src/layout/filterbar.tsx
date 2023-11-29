637208eee4a7cf11df434c8917bd082193593e6a
tad581 <nguyendat58211@gmail.com>
2023-11-29 23:30:25 +0700

    UI: homepage fitlerbar

---
+++ src/layout/filterbar.tsx
@@ -0,0 +1,131 @@
+import { Box, Typography } from '@mui/material';
+import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+
+export default function Filterbar() {
+  return (
+    <Box component='div' sx={{ marginTop: 2 }}>
+      <Box
+        component='div'
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'relative',
+        }}
+      >
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginX: 3,
+          }}
+        >
+          <Box
+            component='img'
+            src='/icons/house.png'
+            sx={{ height: 40, width: 'auto' }}
+          />
+          <Typography
+            component='p'
+            sx={{
+              fontWeight: 700,
+              fontSize: '22px',
+              lineHeight: '30.26px',
+              color: 'gray',
+            }}
+          >
+            Nhà trọ
+          </Typography>
+        </Box>
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginX: 3,
+          }}
+        >
+          <Box
+            component='img'
+            src='/icons/apart.png'
+            sx={{ height: 40, width: 'auto' }}
+          />
+          <Typography
+            component='p'
+            sx={{
+              fontWeight: 700,
+              fontSize: '22px',
+              lineHeight: '30.26px',
+              color: 'gray',
+            }}
+          >
+            Chung cư
+          </Typography>
+        </Box>
+        <Box
+          component='div'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginX: 3,
+          }}
+        >
+          <Box
+            component='img'
+            src='/icons/homestay.png'
+            sx={{ height: 40, width: 'auto' }}
+          />
+          <Typography
+            component='p'
+            sx={{
+              fontWeight: 700,
+              fontSize: '22px',
+              lineHeight: '30.26px',
+              color: 'gray',
+            }}
+          >
+            HomeStay
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: '#DCDCDC',
+            paddingY: 1,
+            paddingX: 2,
+            width: 'fit-content',
+            height: 'auto',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            borderRadius: 10,
+            position: 'absolute',
+            top: '10%',
+            right: '10%',
+          }}
+        >
+          <FilterAltOutlinedIcon sx={{ color: 'gray', marginRight: 1 }} />
+          <Typography
+            component='p'
+            sx={{
+              fontWeight: 700,
+              fontSize: '22px',
+              lineHeight: '30.26px',
+              color: 'gray',
+            }}
+          >
+            Bộ lọc
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,12 @@
+import { Box } from '@mui/material';
+import Filterbar from '../../layout/filterbar';
 import Header from '../../layout/header';
 
 export default function Home() {
   return (
-    <div>
+    <Box sx={{ height: '100vh', width: '100%', backgroundColor: '#F5F5F5' }}>
       <Header />
-    </div>
+      <Filterbar />
+    </Box>
   );
 }
