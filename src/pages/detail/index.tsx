e3e29ce31acadc01e59f414dc6203acd7fac6615
hahunavth <ha.vt194547@sis.hust.edu.vn>
2023-12-08 22:44:22 +0700

    feat: add rating to detail page

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,23 +1,43 @@
-import { Avatar, Box, Button, Container, Divider, Grid, ImageList, ImageListItem, Typography } from "@mui/material";
+import { Avatar, Box, Button, Container, Divider, Grid, ImageList, ImageListItem, Rating, Typography } from "@mui/material";
 import Header from "../../layout/header";
 import Star from '@mui/icons-material/Star';
 import Home from '@mui/icons-material/HomeOutlined';
-
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import { Collapse } from '@mui/material';
 
 
 function Item(props: any) {
   const Icon = props.icon
   return (
-    <Grid item xs={6} display={'flex'} justifyContent={'space-between'} alignItems={'center'} paddingX={10}>
+    <Grid item xs={6} display={'flex'} justifyContent={'start'} alignItems={'center'} paddingX={5} my={1}>
       {/* <Item>1</Item> */}
       <Icon sx={{ fontSize: 30 }}></Icon>
       {/* center text */}
-      <Typography variant="subtitle1" component="span">
+      <Typography variant="subtitle1" component="span" ml={4}>
         {props.children}
       </Typography>
     </Grid>
+  )
+}
+
+function Comment() {
+  return (
+    <Box sx={{ border: 1, padding: 2, borderRadius: 4, minWidth: 400, }}>
+      <Box display={"flex"} flexDirection={"row"}>
+        <Avatar
+          alt="Duy Trọng"
+          src="/static/images/avatar/1.jpg"
+          sx={{ width: 56, height: 56 }}
+        />
+        <Box marginLeft={2}>
+          <Typography variant="h6" color="blue">Duy Trọng</Typography>
+          <Typography variant="subtitle1">16:15 05/09/2023</Typography>
+        </Box>
+      </Box>
+      <Divider light />
+      <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"} marginTop={2}>
+        <Typography variant="subtitle1">Chủ nhà đẹp trai, dễ thương, cảm ơn ""</Typography>
+      </Box>
+    </Box>
   )
 }
 
@@ -79,35 +99,143 @@ export default function Detail() {
 
         {/* description */}
         <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: 4, marginX: "auto", maxWidth: 1000 }}>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ maxWidth: "sm" }}>
-            <Item icon={Home}>Chung cư mini</Item>
-            <Item icon={Home}>Nvs riêng</Item>
-            <Item icon={Home}>Cách ĐHBKHN 3km</Item>
-            <Item icon={Home}>3k / số </Item>
-            <Item icon={Home}>Thanh Xuân, Hà Nội</Item>
-            <Item icon={Home}>35m2</Item>
-          </Grid>
-          <Box sx={{ border: 1, padding: 2, borderRadius: 4, width: 500 }}>
-            <Box display={"flex"} flexDirection={"row"}>
-              <Avatar
-                alt="Duy Trọng"
-                src="/static/images/avatar/1.jpg"
-                sx={{ width: 56, height: 56 }}
+          <Box>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ maxWidth: "sm" }}>
+              <Item icon={Home}>Chung cư mini</Item>
+              <Item icon={Home}>Nvs riêng</Item>
+              <Item icon={Home}>Cách ĐHBKHN 3km</Item>
+              <Item icon={Home}>3k / số </Item>
+              <Item icon={Home}>Thanh Xuân, Hà Nội</Item>
+              <Item icon={Home}>35m2</Item>
+              <Item icon={Home}>35m2</Item>
+              <Item icon={Home}>35m2</Item>
+            </Grid>
+
+            {/* Rating */}
+            <Typography variant="h4" component="h4" marginTop={4}>
+              Nhận xét mới nhất:
+            </Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Typography variant="h4" component="h4" sx={{ fontWeight: 600 }}>
+                5.0 /<span style={{ fontSize: 16, fontWeight: 400 }}>5.0</span>
+                <Rating name="read-only" value={3.5} readOnly sx={{ marginX: 2 }} />
+              </Typography>
+              <Button variant="text" color="inherit" >
+                Đọc tất cả đánh giá
+              </Button>
+            </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Typography variant="subtitle1" component="span">
+                25 đánh giá
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                display: 'flex',
+                gap: 1,
+                py: 1,
+                overflow: 'auto',
+                width: 600,
+                scrollSnapType: 'x mandatory',
+                '& > *': {
+                  scrollSnapAlign: 'center',
+                },
+                '::-webkit-scrollbar': { display: 'none' },
+              }}
+            >
+              <Comment></Comment>
+              <Comment></Comment>
+              <Comment></Comment>
+            </Box>
+
+            <Divider />
+
+
+            <Typography variant="h4" component="h4" marginTop={4}>
+              Chi tiết chỗ ở:
+            </Typography>
+            <Collapse in={open} timeout="auto"
+              unmountOnExit>
+
+              <Typography>Thông tin cá nhân</Typography>
+              <Typography>Giá phòng đã bao gồm</Typography>
+              <Typography>- Một phòng ngủ khép kín có đầy đủ đồ dùng</Typography>
+            </Collapse>
+
+            <Divider />
+
+            <Typography variant="h4" component="h4" marginTop={4}>
+              Tiện nghi:
+            </Typography>
+
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ maxWidth: "sm" }}>
+              <Item icon={Home}>Chung cư mini</Item>
+              <Item icon={Home}>Nvs riêng</Item>
+              <Item icon={Home}>Cách ĐHBKHN 3km</Item>
+              <Item icon={Home}>3k / số </Item>
+              <Item icon={Home}>Thanh Xuân, Hà Nội</Item>
+              <Item icon={Home}>35m2</Item>
+              <Item icon={Home}>35m2</Item>
+              <Item icon={Home}>35m2</Item>
+            </Grid>
+
+            <Divider />
+
+            <Typography variant="h4" component="h4" marginTop={4}>
+              Địa chỉ:
+            </Typography>
+            <Typography variant="subtitle1" component="span">
+              Thanh Xuân, Hà Nội
+            </Typography>
+
+            {/* map */}
+            <Box sx={{ height: 400, width: '100%', marginTop: 4 }}>
+              <iframe
+                width="100%"
+                height="100%"
+                loading="lazy"
+                allowFullScreen
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-3JYr1Tq8LJ2eY9JYR7Xr2eG5J2q9J5c&q=Space+Needle,Seattle+WA"
               />
-              <Box marginLeft={2}>
-                <Typography variant="h6" color="blue">Duy Trọng</Typography>
-                <Typography variant="subtitle1">Môi giới</Typography>
+              </Box>
+
+              <Divider />
+
+              <Typography variant="h4" component="h4" marginTop={4}>
+                Phòng khác của chủ nhà:
+              </Typography>
+          </Box>
+
+          {/* Avatar */}
+          <Box sx={{ maxWidth: 360 }}>
+            <Box sx={{ border: 1, padding: 2, borderRadius: 4 }}>
+              <Box display={"flex"} flexDirection={"row"}>
+                <Avatar
+                  alt="Duy Trọng"
+                  src="/static/images/avatar/1.jpg"
+                  sx={{ width: 56, height: 56 }}
+                />
+                <Box marginLeft={2}>
+                  <Typography variant="h6" color="blue">Duy Trọng</Typography>
+                  <Typography variant="subtitle1">Môi giới</Typography>
+                </Box>
+              </Box>
+              <Divider light />
+              <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"} marginTop={2}>
+                <Button variant="contained" color="inherit" sx={{ maxHeight: '30px', borderRadius: 100, margin: 1 }}>Anh chị có onl k ạ?</Button>
+                <Button variant="contained" color="inherit" sx={{ maxHeight: '30px', borderRadius: 100, margin: 1 }}>Thời hạn thuê tối đa là bao lâu</Button>
+              </Box>
+              <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                <Button variant="contained" color="success" sx={{ borderRadius: 2, margin: 1 }}>Chat với người bán</Button>
               </Box>
             </Box>
-            <Divider light />
-            <Box display={"flex"} flexDirection={"row"} flexWrap={"wrap"} marginTop={2}>
-              <Button variant="contained" color="inherit" sx={{maxHeight: '30px', borderRadius: 100, margin: 1}}>Anh chị có onl k ạ?</Button>
-              <Button variant="contained" color="inherit" sx={{maxHeight: '30px', borderRadius: 100, margin: 1}}>Thời hạn thuê tối đa là bao lâu</Button>
-            </Box>
-            <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
-              <Button variant="contained" color="success" sx={{borderRadius: 2, margin: 1}}>Chat với người bán</Button>
-            </Box>
           </Box>
+        </Box>
+
+        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", marginTop: 4, marginX: "auto", maxWidth: 1000 }}>
+
+
         </Box>
         {/*  */}
       </Container>
